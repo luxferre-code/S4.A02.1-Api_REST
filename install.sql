e4a0bbb313d170964bb5da88770ee8ba942be410
Valentin Thuillier <valentin.thuillier.etu@univ-lille.fr>
2024-02-20 10:30:43 +0100

    [SQL] Création des tables permettant de stocker des pizzas

--- install.sql
+++ install.sql
@@ -1,4 +1,6 @@
-DROP TABLE IF EXISTS ingredient;
+DROP TABLE IF EXISTS ingredient CASCADE;
+DROP TABLE IF EXISTS pizza CASCADE;
+DROP TABLE IF EXISTS compose CASCADE;
 
 CREATE TABLE ingredient (
     id SERIAL,
@@ -6,3 +8,30 @@ CREATE TABLE ingredient (
     prix DECIMAL(5, 2),
     CONSTRAINT ingredient_pk PRIMARY KEY (id)
 );
+
+INSERT INTO ingredient (nom, prix) VALUES ('tomate', 1.5);
+INSERT INTO ingredient (nom, prix) VALUES ('mozzarella', 2);
+INSERT INTO ingredient (nom, prix) VALUES ('jambon', 2.5);
+INSERT INTO ingredient (nom, prix) VALUES ('pate a pizza', 0.5);
+
+CREATE TABLE pizza (
+    id SERIAL,
+    nom TEXT,
+    pate INT,
+    prixBase DECIMAL(5, 2),
+    CONSTRAINT pizza_pk PRIMARY KEY (id),
+    CONSTRAINT pizza_pate_fk FOREIGN KEY (pate) REFERENCES ingredient(id) ON DELETE CASCADE
+);
+
+INSERT INTO pizza(nom, pate, prixBase) VALUES ('Margherita', 4, 6.00);
+
+CREATE TABLE compose (
+    pizza INT,
+    ingredient INT,
+    CONSTRAINT compose_pk PRIMARY KEY (pizza, ingredient),
+    CONSTRAINT compose_pizza_fk FOREIGN KEY (pizza) REFERENCES pizza(id) ON DELETE CASCADE,
+    CONSTRAINT compose_ingredient_fk FOREIGN KEY (ingredient) REFERENCES ingredient(id) ON DELETE CASCADE
+);
+
+INSERT INTO compose(pizza, ingredient) VALUES (1, 1);
+INSERT INTO compose(pizza, ingredient) VALUES (1, 2);
