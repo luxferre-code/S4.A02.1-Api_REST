61a02e5f547feb084887a052ebf12a41ec884cf4
Valentin Thuillier <valentin.thuillier.etu@univ-lille.fr>
2024-02-22 10:41:29 +0100

    Add POST method to add ingredient to a pizza

--- WEB-INF/src/PizzaREST.java
+++ WEB-INF/src/PizzaREST.java
@@ -3,6 +3,7 @@ import fr.valentinthuillier.sae.dao.ComposeDaoSQL;
 import fr.valentinthuillier.sae.dao.IDao;
 import fr.valentinthuillier.sae.dao.IngredientDaoSQL;
 import fr.valentinthuillier.sae.dao.PizzaDaoSQL;
+import fr.valentinthuillier.sae.dto.Compose;
 import fr.valentinthuillier.sae.dto.Ingredient;
 import fr.valentinthuillier.sae.dto.Pizza;
 import jakarta.servlet.ServletException;
@@ -15,6 +16,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet("/pizzas/*")
 public class PizzaREST extends HttpServlet {
@@ -103,31 +105,82 @@ public class PizzaREST extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //TODO: manque POST /pizzas/{id} ajout d’un ingrédient à une pizza
         String json = new BufferedReader(new InputStreamReader(req.getInputStream())).readLine();
         System.out.println(json);
         if(json == null || json.isBlank()) {
             resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid JSON");
             return;
         }
+
+        String info = req.getPathInfo();
+        if(info == null) {
+            info = "";
+        }
+        String[] parts = info.split("/");
+
+        if(parts[0].isEmpty()) {
+            String[] newParts = new String[parts.length - 1];
+            System.arraycopy(parts, 1, newParts, 0, parts.length - 1);
+            parts = newParts;
+        }
+        System.out.println(List.of(parts));
+
         ObjectMapper objectMapper = new ObjectMapper();
         Pizza pizza = null;
-        try {
-            pizza = objectMapper.readValue(json, Pizza.class);
-        } catch(Exception e) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid JSON");
-            return;
-        }
         IDao<Pizza> dao = new PizzaDaoSQL();
-        try {
-            if(!dao.save(pizza)) {
-                resp.sendError(HttpServletResponse.SC_CONFLICT, "Pizza already exists");
-                return;
-            }
-            resp.setStatus(HttpServletResponse.SC_CREATED);
-        } catch(Exception e) {
-            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+
+        switch (parts.length) {
+            case 0:
+                try {
+                    pizza = objectMapper.readValue(json, Pizza.class);
+                } catch(Exception e) {
+                    resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid JSON");
+                    return;
+                }
+                try {
+                    if(!dao.save(pizza)) {
+                        resp.sendError(HttpServletResponse.SC_CONFLICT, "Pizza already exists");
+                        return;
+                    }
+                    resp.setStatus(HttpServletResponse.SC_CREATED);
+                } catch(Exception e) {
+                    resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+                }
+                break;
+            case 1:
+                int id = IngredientREST.parseInt(parts[0]);
+                if(id == -1) {
+                    resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid id");
+                    return;
+                }
+                pizza = dao.findById(id);
+                if(pizza == null) {
+                    resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Pizza not found");
+                    return;
+                }
+                IDao<Ingredient> ingredientDao = new IngredientDaoSQL();
+                Ingredient ingredient = null;
+                try {
+                    ingredient = objectMapper.readValue(json, Ingredient.class);
+                } catch(Exception e) {
+                    resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid JSON");
+                    return;
+                }
+                if(ingredientDao.findById(ingredient.getId()) == null) {
+                    resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Ingredient not found");
+                    return;
+                }
+                ComposeDaoSQL composeDao = new ComposeDaoSQL();
+                if(composeDao.save(new Compose(pizza.getId(), new Ingredient[] { ingredient }))) {
+                    resp.setStatus(HttpServletResponse.SC_CREATED);
+                } else {
+                    resp.sendError(HttpServletResponse.SC_CONFLICT, "Ingredient already exists in pizza");
+                }
+                break;
+            default:
+                break;
         }
+        
     }
 
     @Override
